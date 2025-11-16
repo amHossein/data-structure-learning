2660664957046acaf59827415a7238b8d42cca3d
amHossein <hosseinhastam1999@gmail.com>
2025-11-16 20:03:47 +0330

    modify example

--- Recursion/eg_sum_of_first_n_natural_numbers.cpp
+++ Recursion/eg_sum_of_first_n_natural_numbers.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 
-// NOTE the formula is more efficient: (n*(n-1))/ 2
-int sum(int n)
+int recursion_sum(int n)
 {
     if(n==0)
         return 0;
-    return sum(n-1)+n;
+    return recursion_sum(n-1)+n;
 }
+int iteration_sum(int n)
+{
+    int sum = 0;
+
+    for(int i=1; i<=n; i++)
+        sum = sum + i;
+    
+    return sum;
+}
+// NOTE the formula is more efficient: (n*(n-1))/ 2
+int formula_sum(int n)
+{
+    return (n*(n+1)) / 2;
+}
+
 int main()
 {
-    std::cout<<sum(5);
+    std::cout<<recursion_sum(5) << std::endl;
+    std::cout<<iteration_sum(5) << std::endl;
+    std::cout<<formula_sum(5);
     return 0;
 }
