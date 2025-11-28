ee5e7dcda9ad23458b2ed090967ce88c9a838e91
amHossein <hosseinhastam1999@gmail.com>
2025-11-28 07:40:35 +0330

    add taylor_iter func to eg_taylor_series.cpp

--- Recursion/eg_taylor_series.cpp
+++ Recursion/eg_taylor_series.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+double taylor_iter(int x, int n)
+{
+    double s = 1;
+    int i; 
+    double num=1;
+    double den=1;
 
+    for(i=1; i<n; i++)
+    {
+        num*=x;
+        den*=i;
+        s+=num/den;
+    }
+
+    return s;
+}
 double taylor(int x, int n)
 {
     static double p=1, f=1;
@@ -18,6 +33,7 @@ double taylor(int x, int n)
 
 int main()
 {
-    std::cout<< taylor(1,20);
+    std::cout<< taylor(1,20) << std::endl;
+    std::cout<< taylor_iter(1,20);
     return 0;
 }
